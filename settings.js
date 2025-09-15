ef6bbeaeee952ff20710be825b7af0ad20035b0f
Slownam <156661263+Slownam@users.noreply.github.com>
2025-09-16 06:50:35 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,17 +1,13 @@
-// settings.js - edit sesuai kebutuhanmu (WORKING)
-window.Domain = 'https://pterodactyl.idstore.biz.id';       // domain panel (boleh pakai https://)
-window.ptla   = 'ptla_9zvvTw8oXQ1mwY6qQzImyKkEHtNoUirCZ9umbqOYgcy'; // apikey ptero -> apikeyptero
-window.ptlc   = 'ptlc_NlRH6nke3h1xYQuNOUGyzXZtnpdaR7IxIFkNCIlPYUe';     // capikey ptero -> capikeyptero
-
-// default (boleh tetap atau kosong)
+// settings.js - default values (dipakai jika admin tidak mengganti via admin.html)
+window.Domain = 'https://pterodactyl.idstore.biz.id';    // isi panelmu
+window.ptla   = 'ptla_9zvvTw8oXQ1mwY6qQzImyKkEHtNoUirCZ9umbqOYgcy';
+window.ptlc   = 'ptlc_NlRH6nke3h1xYQuNOUGyzXZtnpdaR7IxIFkNCIlPYUe';
 window.egg    = '15';
 window.nestid = '1';
 window.loc    = '1';
-
-// jika perlu agar API membuat sebagai root admin
 window.rootadmin = false;
 
-// export optional untuk Node
+// optional export for Node
 if(typeof module !== 'undefined' && module.exports){
   module.exports = { Domain: window.Domain, ptla: window.ptla, ptlc: window.ptlc, egg: window.egg, nestid: window.nestid, loc: window.loc, rootadmin: window.rootadmin };
 }
